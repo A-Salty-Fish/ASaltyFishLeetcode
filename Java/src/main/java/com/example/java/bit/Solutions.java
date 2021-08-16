8f0cfe70e049e4a4eb1b0dbf0f823db3e895e368
A-Salty-Fish <1309032417@qq.com>
2021-08-16 15:00:03 +0800

    feat: 405. 数字转换为十六进制数

--- Java/src/main/java/com/example/java/bit/Solutions.java
+++ Java/src/main/java/com/example/java/bit/Solutions.java
@@ -10,7 +10,9 @@ import java.util.Arrays;
  * @date 2021/8/16 14:26
  */
 public class Solutions {
-    // 504 七进制数
+    /**
+     * 504 七进制数
+      */
     public String convertToBase7(int num) {
         StringBuilder result = new StringBuilder(64);
         boolean isNegtive = false;
@@ -26,6 +28,31 @@ public class Solutions {
         if (isNegtive) {
             result.append('-');
         }
+        return result.reverse().toString();
+    }
+
+    /**
+     * 405. 数字转换为十六进制数
+     * @param num
+     * @return
+     */
+    public String toHex(int num) {
+        char[] dig2Hex = new char[16];
+        for (int i = 0; i < 10 ; i++) {
+            dig2Hex[i] = (char) ('0' + i);
+        }
+        for (int i = 10; i < 16 ; i++) {
+            dig2Hex[i] = (char) ('a' + i - 10);
+        }
+        long n = num;
+        if (n < 0) {
+            n += (1L << 32L);
+        }
+        StringBuilder result = new StringBuilder(8);
+        do {
+            result.append(dig2Hex[(int)(n%16L)]);
+            n /= 16;
+        } while(n != 0);
         return result.reverse().toString();
     }
 }
